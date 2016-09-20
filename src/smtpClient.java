2e7afdd065710435ef346008991565ee81d7512f
Lars Martin G. Risdal <Lars Martin G. Risdal>
2016-09-20 00:42:47 -0500

    fixed imports

--- src/smtpClient.java
+++ src/smtpClient.java
@@ -1,4 +1,9 @@
-import java.io.*;
+
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.InputStreamReader;
+import java.io.IOException;
 import java.net.Socket;
 import java.net.UnknownHostException;
 
